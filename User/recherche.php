96cdf612ae520d63a6f1813fafbaefc88518e7f6
Timothee Benedetti <tibenede@etu.utc.fr>
2013-06-03 17:42:30 +0200

    recherche partie 1

--- User/recherche.php
+++ User/recherche.php
@@ -8,18 +8,39 @@
 		<title>RealProjet5</title>
 		<link href="css/index.css" rel="stylesheet" type="text/css">
 		<link href="css/header.css" rel="stylesheet" type="text/css">
+		<script src="./recherche.js" type="text/javascript"></script>
 	</head>
 	<body>
-		<?php include('header.php') ?>
+		<?php include('header.php');
+			$vSql = "SELECT distinct aencodage FROM tdocvideos;";
+			$vQuery = pg_query($vConn,$vSql);
+			$res = pg_fetch_array($vQuery);
+			while($res = pg_fetch_array($vQuery)){
+				if (isset($formatsV))
+					$formatsV = $formatsV.','.$res['aencodage'];
+				else
+					$formatsV = $res['aencodage'];
+			}
+			
+			$vSql = "SELECT distinct aencodage FROM tdocsonores;";
+			$vQuery = pg_query($vConn,$vSql);
+			$res = pg_fetch_array($vQuery);
+			while($res = pg_fetch_array($vQuery)){
+				if (isset($formatsA))
+					$formatsA = $formatsA.','.$res['aencodage'];
+				else
+					$formatsA = $res['aencodage'];
+			}
+		?>
 		<h1>RECHERCHE AVANCEE</h1>
 		
 		<form method="POST" action="recherche.php">
 			<span>Titre : <input type="text" name="title" /></span><br/>
 			<span>Type : 
-				<label>Vidéo </label><input type="radio" name="type" value="vid"><br>
-				<label>Sonore </label><input type="radio" name="type" value="son"><br>
-				<label>Texte </label><input type="radio" name="type" value="text"><br>
-				<label>Tout type </label><input type="radio" name="type" value="tous">
+				<label>Vidéo </label><input type="radio" name="type" value="vid" onFocus='changeVideo("suiteform", <?php echo '"'.$formatsV.'"'; ?>);'><br>
+				<label>Sonore </label><input type="radio" name="type" value="son" onFocus='changeSonore("suiteform", <?php echo '"'.$formatsA.'"'; ?>);'><br>
+				<label>Texte </label><input type="radio" name="type" value="text" onFocus='changeTexte("suiteform");'><br>
+				<label>Tout type </label><input type="radio" name="type" value="tous" onFocus='resetDiv("suiteform");'>
 			</span><br/> 	
 		
 			<span>Date de parution : </span>
@@ -31,7 +52,8 @@
 					while($res = pg_fetch_array($vQuery)){
 					echo '<option value="'.$res['date'].'">'.$res['date'].'</option>';
 				}?>
-			</select>
+			</select><br>
+			<div id="suiteform"></div>
 			<br/><br/>
 			<input type="submit" value="Rechercher"/>
 		</form>
